ac9eedbf9e86b8b4b3177186bcb3ae524507fb52
zeeltrivedi1298 <zeeltrivedi@csu.fullerton.edu>
2023-12-22 11:36:53 -0800

    modified code

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,11 +1,10 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-res.send("Hello World");
-});
+
  
 app.listen(port, () => {
     console.log(`video processing service listening at http://localhost:${port}`)
